497187ba8d2e7cbe16c65973d4611d0d3c9bc71b
bayjidmia <mdbayjidmia982@gmail.com>
2026-04-09 19:24:23 +0600

    added app.js

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 import { connectDB } from "./config/db.js";
-import express from "express";
-import cors from "cors";
+import app from "./app.js";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js";
+
 dotenv.config();
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-app.use("/api/user", authRoutes);
 
 connectDB();
 const PORT = process.env.PORT || 5000;
